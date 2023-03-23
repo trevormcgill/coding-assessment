167f6cf6f0836db6209ac41b307f26052e38d728
Trevor McGill <trevor.mcgill@icloud.com>
2023-03-22 20:22:42 -0500

    removed copy of script, updated index

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 var mainEl = document.querySelector("main");
 var startBtnEl = document.querySelector("#start-btn");
-var timerEl = document.querySelector("#time-el")
+var timerEl = document.querySelector("#time-el");
+var saveFormEl = document.querySelector("#save-form");
 
 var interval;
 var time = 100;
@@ -8,36 +9,44 @@ var questionIndex = 0;
 var lastQuestionCorrect;
 var lastQuestionIncorrect;
 
+// create a container for the save form
+var saveFormContainer = document.createElement("div");
+saveFormContainer.id = "save-form-container";
+mainEl.appendChild(saveFormContainer);
+
+// append the save form to the container
+saveFormContainer.appendChild(saveFormEl);
+
 var questions = [
   {
-    questionText: "What is your favorite food?",
-    questionChoices: ["Pizza", "Ice Cream", "Yogurt", "Hot Dogs"],
-    correctAnswer: 0
+    questionText: "What is your favorite color?",
+    questionChoices: ["1. Red", "2. White", "3. Blue"],
+    correctAnswer: 1
   },
   {
     questionText: "What is your favorite food?",
-    questionChoices: ["Pizza", "Ice Cream", "Yogurt", "Hot Dogs"],
+    questionChoices: ["1. Pizza", "2. Ice Cream", "3. Yogurt", "4. Hot Dogs"],
     correctAnswer: 0
   },
   {
-    questionText: "What is your favorite food?",
-    questionChoices: ["Pizza", "Ice Cream", "Yogurt", "Hot Dogs"],
-    correctAnswer: 0
+    questionText: "WHAT IS GOING ON????",
+    questionChoices: ["1. I don't know", "2. okay", "3. AAAHHH", "4. HOT DOG!"],
+    correctAnswer: 2
   },
   {
-    questionText: "What is your favorite food?",
-    questionChoices: ["Pizza", "Ice Cream", "Yogurt", "Hot Dogs"],
-    correctAnswer: 0
+    questionText: "What is your favorite thing?",
+    questionChoices: ["1. Computer", "2. Apple", "3. Overwatch", "4. Path of Exile"],
+    correctAnswer: 2
   },
   {
-    questionText: "What is your favorite food?",
-    questionChoices: ["Pizza", "Ice Cream", "Yogurt", "Hot Dogs"],
-    correctAnswer: 0
+    questionText: "What is your favorite ting?",
+    questionChoices: ["1. You", "2. Me", "3. Nandos", "4. HELP ME"],
+    correctAnswer: 3
   },
   {
-    questionText: "What is your favorite food?",
-    questionChoices: ["Pizza", "Ice Cream", "Yogurt", "Hot Dogs"],
-    correctAnswer: 0
+    questionText: "What is your favorite animal?",
+    questionChoices: ["1. Dog", "2. Monke", "3. Bear", "4. MONKE"],
+    correctAnswer: 1
   },
 ];
 
@@ -48,51 +57,51 @@ function displayQuestion() {
     endGame();
     return;
   }
-  
-  var h1El = document.createElement('h1');
-  h1El.textContent = questions[questionIndex].questionText;
-  mainEl.appendChild(h1El);
-  
-  var btnDivEl = document.createElement("div")
-  mainEl.appendChild(btnDivEl)
 
-  var pEl = document.createElement('p');
+  var btnDivEl = document.createElement("div");
+  mainEl.appendChild(btnDivEl);
+
+  var h1El = document.createElement("h1");
+  h1El.setAttribute("id", "intro");
+  h1El.textContent = questions[questionIndex].questionText;
+  btnDivEl.appendChild(h1El);
+
+  // Reset result variables
+  lastQuestionCorrect = "";
+  lastQuestionIncorrect = "";
+
+  // Append result text to btnDivEl
+  var pEl = document.createElement("p");
   pEl.textContent = lastQuestionCorrect;
-  mainEl.appendChild(pEl);
-  
-  mainEl.addEventListener("click", function(event) {
+  btnDivEl.appendChild(pEl);
+
+  for (var j = 0; j < questions[questionIndex].questionChoices.length; j++) {
+    var buttonEl = document.createElement("button");
+    buttonEl.textContent = questions[questionIndex].questionChoices[j];
+    buttonEl.setAttribute("class", "custom-btn");
+    buttonEl.setAttribute("data-index", j);
+    btnDivEl.appendChild(buttonEl);
+  }
+
+  btnDivEl.addEventListener("click", function (event) {
     var target = event.target;
-  
-    if (target.getAttribute("class") !== 'btn') return;
+
+    if (target.getAttribute("class") !== "custom-btn") return;
 
     var clickedQuestionIndex = parseInt(target.getAttribute("data-index"));
 
     if (clickedQuestionIndex === questions[questionIndex].correctAnswer) {
-      lastQuestionCorrect = "Correct"
+      lastQuestionCorrect = "Correct";
     } else {
       time = time - 10;
-      lastQuestionIncorrect = "Incorrect"
+      lastQuestionIncorrect = "Incorrect";
     }
 
     questionIndex++;
 
     displayQuestion();
-
   });
-
-
-  for (var i = 0; i < questions[questionIndex].questionChoices.length; i++) {
-    var buttonEl = document.createElement('button');
-    buttonEl.textContent = questions[questionIndex].questionChoices[i];
-    buttonEl.setAttribute("class", "btn");
-    btnDivEl.setAttribute("data-index", i);
-    //make another div put buttons in div and style
-    // mainEl.appendChild(buttonEl);
-
-    };
-  }
-
-
+}
 
 // start button
 startBtnEl.addEventListener("click", function (event) {
@@ -116,7 +125,12 @@ startBtnEl.addEventListener("click", function (event) {
 
 // what happens when game is over
 function endGame() {
+  console.log("hit")
   clearInterval(interval);
   //save to local storage
-
-}
+  saveFormContainer.classList.remove("d-none");
+  saveFormContainer.appendChild(saveFormEl);
+  saveFormEl.setAttribute("style", "d-block !important");
+  console.log(document.querySelector('#save-form'));
+  timerEl.setAttribute("style", "display: none !important");
+};
